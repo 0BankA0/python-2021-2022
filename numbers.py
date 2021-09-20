ec6af255005bbcda16e1879f9807e5cd0cfc21f5
0BankA0 <artursmurans64@gmail.com>
2021-09-20 13:52:14 +0300

    new file:   numbers.py

---
+++ numbers.py
@@ -0,0 +1,19 @@
+#mainigas numbers
+num1 = 6 #int
+num2 = 2.5 #Float
+num3 = num1 + num2
+print(num3)
+print(str(num1) + " plus" + str(num2) + " vienads ar", str(num3))
+print(str(num1), "plus", str(num2), "vienads ar", str(num3))
+#ievade
+num1 = int(input("ievadi skaitli: "))
+num2 = int(input("ievadi skaitli: "))
+
+num123 = num1 + num2
+
+
+print(num123)
+
+
+print(f"ja {num1} saskaitīt ar {num2} tas bus {num123}")
+
