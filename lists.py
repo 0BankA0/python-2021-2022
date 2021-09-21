cd076fa8c389b91f434b28700666126b1805ce62
0BankA0 <artursmurans64@gmail.com>
2021-09-21 12:48:33 +0300

    new file:   lists.py

---
+++ lists.py
@@ -0,0 +1,35 @@
+#mainigais saraksts, jeb list
+
+#[]
+
+list=['word', 12 , 222]
+print(list)
+
+#elementu skaits mainigaja
+
+print("elementu skaits", len(list))
+
+#indeks metode
+
+print(list[1])
+print(list[0:])
+
+
+
+#elementu mainja
+list[0] = "word2"
+print(list)
+
+
+#elementu pievienišana
+print(list + ['hey'])
+list = list + ['what']
+print(list)
+
+
+list.append('word3')
+print(list)
+
+#elementa izdešana
+list.pop(2)
+print(list)
