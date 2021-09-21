e2f84be5b4edd20c4022b3da871e99898673eaa5
0BankA0 <artursmurans64@gmail.com>
2021-09-21 13:54:59 +0300

    new file:   tuples_sets.py

---
+++ tuples_sets.py
@@ -0,0 +1,16 @@
+#Tupls
+
+# ()
+
+tuple = ('sleap', 'play', 'work')
+
+print(len(tuple))
+
+print(tuple [2])
+
+print(tuple.index('work'))
+
+tuple= (11, 12, 11, 14, 12, 13)
+
+
+print(tuple.count(11))
