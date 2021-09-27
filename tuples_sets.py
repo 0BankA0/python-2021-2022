028eeb191166331eb6c4320beec1e8b9e5f54b3e
0BankA0 <artursmurans64@gmail.com>
2021-09-27 12:18:12 +0300

    modified:   tuples_sets.py

--- tuples_sets.py
+++ tuples_sets.py
@@ -10,7 +10,19 @@ print(tuple [2])
 
 print(tuple.index('work'))
 
-tuple= (11, 12, 11, 14, 12, 13)
+tuple= (11, 12, 11, 14, 12, 13, 11, 11)
+
 
 
 print(tuple.count(11))
+
+
+#set
+
+# {}
+
+
+s = set()
+s.add(1)
+
+print(s)
