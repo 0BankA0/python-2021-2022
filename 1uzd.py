7db30f7d39825715b962031680c2db137ee5b2cf
0BankA0 <artursmurans64@gmail.com>
2021-09-27 12:52:10 +0300

    new file:   1uzd.py

---
+++ 1uzd.py
@@ -0,0 +1,43 @@
+#pirmais punktu bloks
+
+aaa=("bbbbb")
+
+print(aaa)
+
+aaa='bbbbb'
+print(aaa)
+
+#otrais punktu bloks
+aaa= "453554452"
+
+print (aaa)
+
+#tršais punktu bloks
+
+a = int(2)
+b = float(2.5)
+c = a + b
+
+print(str(c))
+
+#centurtais punktu bloks
+
+print(f"{a} plus {b} bus {c}")
+
+#piektais punktu bloks
+
+
+aaaa=["pirmais", 2, 3, 10]
+
+print("elementi", len(aaaa))
+
+#septitais punktu bloks
+
+print(aaaa + ['kautkas'])
+
+#astotais punktu bloks
+
+aaaa[2]= 123412
+
+print(aaaa)
+
