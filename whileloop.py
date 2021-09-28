8c2a2e8e77736c9dbbd0f124e9c7508c94d63db3
0BankA0 <artursmurans64@gmail.com>
2021-09-28 13:26:28 +0300

    new file:   whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,32 @@
+'''
+Artūrs Murāns
+
+28.09.2021
+
+'''
+
+number = 1
+while number > 1:
+    print(number)
+    if number == 100:
+        break
+    loop = +- 1
+
+
+number = number
+
+while number < 100:
+    number += 1
+    if number == 99:
+        continue
+    print(number)
+
+number = 1
+while number < 69:
+    print(number)
+    number += 1
+else:
+    print(number)
+    print('cipars')
+
+
