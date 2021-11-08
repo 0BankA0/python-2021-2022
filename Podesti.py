f15907be1902005318dc257edf31ac2316f6a589
0BankA0 <artursmurans64@gmail.com>
2021-11-08 14:00:29 +0200

    new file:   Podesti.py

---
+++ Podesti.py
@@ -0,0 +1,33 @@
+def MaterialuUzkaite(tips, izmers1, izmers2, skaits):
+    pass
+
+
+
+
+
+def MaterialuApreikins(platums, garums, augstums, skaits):
+    
+    
+    MaterialuUzkaite('Finieris', platums, garums, 2*skaits)
+
+    MaterialuUzkaite('Finieris', garums, augstums, 2*skaits)
+
+    MaterialuUzkaite('Fineris', platums, augstums, 2*skaits)
+    
+   
+   
+   
+    MaterialuUzkaite('Liste', garums, 0,  4*skaits)
+
+    MaterialuUzkaite('Liste',platums, 0, 4*skaits)
+
+    MaterialuUzkaite('Liste', augstums, 0, 4*skaits)
+
+
+
+
+    MaterialuUzkaite('Lenkis', 0, 0, 8*skaits)
+
+
+
+
